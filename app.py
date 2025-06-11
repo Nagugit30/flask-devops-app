8d34b5eb0e195f34a4be8eb31d35dc57d2014c61
Nagarajan  Santharam <nagaraj.rajan@redingtongroup.com>
2025-06-11 12:32:48 +0530

    Intial commit -Flask Devops App

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "✅ Hello from your DevOps Flask App!"
+
+@app.route('/health')
+def health():
+    return "OK", 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
